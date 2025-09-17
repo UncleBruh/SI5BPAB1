97121ed2b0535efa8d08fece73a1b2dc3d2277fb
Nelson <74392133+UncleBruh@users.noreply.github.com>
2025-09-17 15:08:28 +0700

    Latihan 1

--- latihan_pertemuan_2/bin/latihan_pertemuan_2.dart
+++ latihan_pertemuan_2/bin/latihan_pertemuan_2.dart
@@ -1,5 +1,16 @@
-import 'package:latihan_pertemuan_2/latihan_pertemuan_2.dart' as latihan_pertemuan_2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${latihan_pertemuan_2.calculate()}!');
+void main(){
+  var totalBelanja = 550000;
+  if (totalBelanja > 500000) {
+    var disc = 0.8;
+    print("Selamat Anda mendapatkan diskon 20%");
+    print("Total Belanja Anda adalah ${totalBelanja*0.8}");
+  } else if (totalBelanja > 200000) {
+    var disc = 0.9;
+    print("Selamat Anda mendapatkan diskon 10%");
+    print("Total Belanja Anda adalah ${totalBelanja*0.9}");
+  } else {
+    var disc = 1;
+    print("Maaf, Anda tidak mendapatkan diskon");
+    print("Total Belanja Anda adalah $totalBelanja");
+  }
 }
