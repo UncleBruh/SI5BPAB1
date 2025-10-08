d733fb85180142bfa4b34db73c060ff10a00eec6
Nelson <74392133+UncleBruh@users.noreply.github.com>
2025-10-08 14:09:00 +0700

    Repository

--- latihan_pertemuan_2/test/latihan_pertemuan_2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:latihan_pertemuan_2/latihan_pertemuan_2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- pertemuan_2/test/pertemuan_2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:pertemuan_2/pertemuan_2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
